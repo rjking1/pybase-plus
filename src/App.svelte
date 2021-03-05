48536071743f7b330c9c336539b6bbd3df50a4a8
Richard King <richard@renewableenergyhub.com.au>
2021-03-05 13:14:10 +1100

    query via email news view

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,8 @@
     import { doFetch } from './common.js'
 
 	let db = 'art25285_hut'
-	let sql = "select * from members limit 5" 
+	let sql = "select * from members where LASTNAME = 'King'" 
+	let viewName = "News Email"
 
 	let sender = "heather@artspace7.com.au"
 	let subject = "Test"
@@ -21,20 +22,28 @@
 		console.log(result)
 	}
 
+	async function doView() {
+		let rows = await doFetch(db, "SELECT get_sql FROM py_views WHERE name = '" + viewName + "'")
+		sql = rows[0]["get_sql"]
+		result = await doFetch(db, sql)
+		console.log(result)
+	}
+
 	async function doSend() {
-		result.forEach(element => {
-			
-		Email.send({
-			Host : "mail.artspace7.com.au",
-			Username : "art25285",
-			Password : "ear724gni",
-			To : element["EMAIL"],
-			From : sender,
-			Subject : subject,
-			Body : contents
-		}).then(
-		message => console.log(message)
-		);
+		result.forEach(row => {
+			if(row["EMAIL"]) {			
+				Email.send({
+					Host : "mail.artspace7.com.au",
+					Username : "art25285",
+					Password : "ear724gni",
+					To : row["EMAIL"],
+					From : sender,
+					Subject : subject,
+					Body : contents
+				}).then(
+					message => console.log(message)
+				); 
+			}
 		});
 	}
 
@@ -48,10 +57,10 @@
 
 <label>db</label><input bind:value={db} />
 <label>sql</label><input bind:value={sql} />
+<label>viewName</label><input bind:value={viewName} />
 <br>
-<button type="submit" on:click={doQuery}>
-	Query
-</button>
+<button type="submit" on:click={doQuery}>SQL Query</button>
+<button type="submit" on:click={doView}>View Query</button>
 <p><b>Email</b></p>
 <label>From</label><input bind:value={sender} />
 <label>Subject</label><input bind:value={subject} />
@@ -60,20 +69,6 @@
 <button type="submit" on:click={doSend}>
 	Send Emails
 </button>
-
-<!-- <pre>
-{#if result}
-<ul>
-	{#each result as row}
-		<li>
-			{#each Object.values(row) as field}
-				{field + ' '}
-			{/each}
-		</li>
-	{/each}
-</ul> 
-{/if}
-</pre> -->
 
 <pre>
 {#if result}
