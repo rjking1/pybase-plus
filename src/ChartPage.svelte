ad582c5117c84e7899b001b5fbd2530d3598bf34
Richard King <richard@renewableenergyhub.com.au>
2021-05-09 18:28:47 +1000

    bar and dot charts

--- src/ChartPage.svelte
+++ src/ChartPage.svelte
@@ -9,7 +9,7 @@
     viewDetail,
     writeAuditText,
   } from "./Common.js";
-  import Chart from 'svelte-frappe-charts'
+  import Chart from "svelte-frappe-charts";
 
   let p;
   let v;
@@ -20,11 +20,12 @@
   let data = null;
   let aresult = null;
 
-    let heatmapDataKms = null
-  let heatmapDataAlt = null
-  let heatmapColorsKms
-  let heatmapColorsAlt
-
+  let dates = null;
+  let dots = null;
+  let heatmapDataKms = null;
+  let heatmapDataAlt = null;
+  let heatmapColorsKms;
+  let heatmapColorsAlt;
 
   // a lot of this should go into common as membersPage and CalendarPage have thee 3 functions in common
   onMount(async () => {
@@ -40,7 +41,6 @@
   });
 
   async function doChart() {
-
     fields = JSON.parse(v.fields);
     if (fields === null) {
       fields = [];
@@ -50,12 +50,63 @@
     let sql = v.get_sql.replace("%d", p.id);
     data = await doFetch($dbN, sql);
 
-    if(v.formDesc == "!chart bar") {
-      doBarChart()
+    if (v.formDesc == "!chart bar") {
+      doBarChart();
     }
-    if(v.formDesc == "!heatmap") {
-      doHeatmap()
+    if (v.formDesc == "!chart dots") {
+      doDotChart();
     }
+    if (v.formDesc == "!heatmap") {
+      doHeatmap();
+    }
+  }
+
+  function doBarChart() {
+    let x = [];
+    let y1 = [];
+    let y2 = [];
+    data.forEach((item) => {
+      // item=row
+      console.log(item);
+      // we don't care what the col names are
+      // first is usually date on x axis
+      // second+ are y values
+      x.push(Object.values(item)[0]);
+      y1.push(Object.values(item)[1]);
+      y2.push(Object.values(item)[2]);
+    });
+
+    dates = {
+      labels: x,
+      datasets: [
+        { name: "Km",  values: y1, chartType: 'bar' }, // type only nec if multi-axis
+        { name: "Alt", values: y2, chartType: 'bar' }
+      ],
+    };
+  }
+
+  function doDotChart() {
+    let x = [];
+    let y1 = [];
+    let y2 = [];
+    data.forEach((item) => {
+      // item=row
+      console.log(item);
+      // we don't care what the col names are
+      // first is usually date on x axis
+      // second+ are y values
+      x.push(Object.values(item)[0]);
+      y1.push(Object.values(item)[1]);
+      y2.push(Object.values(item)[2]);
+    });
+
+    dots = {
+      labels: y1,
+      datasets: [
+        // { name: "Km",  values: y1, chartType: 'bar' }, // type only nec if multi-axis
+        { name: "Alt", values: y2 }
+      ],
+    };
   }
 
   function doHeatmap() {
@@ -63,38 +114,59 @@
     // let data = await doFetch(
     //   'select ride_date, sum(km) as sumkm, sum(alt_gain) as sumalt from rides where ride_date >= curdate() - interval 1 year group by 1 order by 1',
     // )
-    let dateKms = {}
-    let dateAlt = {}
-    data.forEach((item) => (dateKms[item.ride_date] = item.sumkm))
-    data.forEach((item) => (dateAlt[item.ride_date] = item.sumalt))
+    let dateKms = {};
+    let dateAlt = {};
+    data.forEach((item) => (dateKms[item.ride_date] = item.sumkm));
+    data.forEach((item) => (dateAlt[item.ride_date] = item.sumalt));
     // console.log(dateKms)
-    let today = new Date() 
-    today.setDate(today.getDate() + 1)
-    let t365 = new Date()
-    t365.setDate(t365.getDate() - 366)
+    let today = new Date();
+    today.setDate(today.getDate() + 1);
+    let t365 = new Date();
+    t365.setDate(t365.getDate() - 366);
 
-    heatmapColorsKms = ['#ebedf0', '#c6e48b', '#7bc96f', '#239a3b', '#196127']
+    heatmapColorsKms = ["#ebedf0", "#c6e48b", "#7bc96f", "#239a3b", "#196127"];
     heatmapDataKms = {
       dataPoints: dateKms,
       start: t365,
       end: today,
-    }
-    heatmapColorsAlt = ['#ebedf0', '#c0ddf9', '#73b3f3', '#3886e1', '#17459e']
+    };
+    heatmapColorsAlt = ["#ebedf0", "#c0ddf9", "#73b3f3", "#3886e1", "#17459e"];
     heatmapDataAlt = {
       dataPoints: dateAlt,
       start: t365,
       end: today,
-    }
+    };
   }
 
+  let lineOptions = {hideLine: 1}
 </script>
+
+<h4>Chart</h4>
+{#if dates}
+  <Chart data={dates} type="bar" />
+  <!-- height="150px" /> -->
+{/if}
+{#if dots}
+  <Chart data={dots} type="line" lineOptions={lineOptions}} />
+  <!-- height="150px" /> -->
+{/if}
 
 <h4>Km</h4>
 {#if heatmapDataKms}
-  <Chart data={heatmapDataKms} type="heatmap" colors={heatmapColorsKms} height=150px />
+  <Chart
+    data={heatmapDataKms}
+    type="heatmap"
+    colors={heatmapColorsKms}
+    height="150px"
+  />
 {/if}
 
 <h4>Alt (m)</h4>
 {#if heatmapDataAlt}
-  <Chart data={heatmapDataAlt} type="heatmap" colors={heatmapColorsAlt} height=160px />
+  <Chart
+    data={heatmapDataAlt}
+    type="heatmap"
+    colors={heatmapColorsAlt}
+    height="160px"
+  />
 {/if}
