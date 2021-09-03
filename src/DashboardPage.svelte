f554d08cb0349ac83fdbc4b588e4c465c8e4e341
Richard King <rjking58@gmail.com>
2021-09-03 20:52:32 +1000

    added notes and experimental use of a view

--- src/DashboardPage.svelte
+++ src/DashboardPage.svelte
@@ -35,12 +35,15 @@
   }
 
   async function doUpdateAll() {
-
     const matches = document.querySelectorAll("[id^='sql']");
-    let sql = matches[0].dataset.sql // special function: reads data-sql attribute
-    // console.log(sql); 
+    let sql = matches[0].dataset.sql; // special function: reads data-sql attribute
+    // console.log(sql);
+
+    // should the sql matches result drive what we populate in the dashboard?
+    // do the fields need to be prefixed with the sql id/number?
+
     let result = await doGetResult(sql);
-    //   `select concat(p.regionid, "_price") as "key", rrp as "value" from DISPATCH__PRICE p 
+    //   `select concat(p.regionid, "_price") as "key", rrp as "value" from DISPATCH__PRICE p
     //   where p.settlementdate >= (select max(settlementdate) from DISPATCH__PRICE)`
     // );
     // console.log(result);
@@ -50,13 +53,18 @@
       addWidget("#" + reg + "_price", "x");
     }
 
-    sql = matches[1].dataset.sql // special function: reads data-sql attribute
-    // console.log(sql); 
+    sql = matches[1].dataset.sql; // special function: reads data-sql attribute
+    // console.log(sql);
+
+    /// we already have all the sql in memory in the $views store
+    // so should allow a view name to be used
+    // which avoids needing to have a copy in the html
+    // but having sql in the html as an option allows for max flexibility
 
     result = await doGetResult(sql);
     //   `select p.regionid as "Region", format(rrp, 2) as "RRP", format(clearedsupply,0) as "Demand",
     //   format(dispatchablegeneration,0) as "Generation" ,
-    //   format(availablegeneration,0) as "Available"  
+    //   format(availablegeneration,0) as "Available"
     //   from DISPATCH__PRICE p join DISPATCH__REGIONSUM r
     //   on p.settlementdate = r.settlementdate and p.regionid = r.regionid
     //   where p.settlementdate >= (select max(settlementdate) from DISPATCH__PRICE) - interval 1 minute
@@ -66,9 +74,19 @@
     addTableWidget("#t1", result);
 
     // an advantage of not passing a view into the table or chart widgets is that we can share the result
+    // tho I'm not doing that here !
 
-    result = await doGetResult('select regionid as "_", settlementdate, rrp as "rrp_" from DISPATCH__PRICE where settlementdate >= now() - interval 1 hour');
-    addChartWidget("#c1", result)
+    result = await doGetResult(
+      'select regionid as "_", settlementdate, rrp as "rrp_" from DISPATCH__PRICE where settlementdate >= now() - interval 1 hour'
+    );
+    addChartWidget("#c1", result);
+
+    // need a way of specifying a view name in the html id="view-1"?
+
+    sql = viewDetail($views, "09 Current gen").get_sql;
+    console.log(sql);
+    result = await doGetResult(sql);
+    addChartWidget("#c2", result);
   }
 
   function lookup(rows, key) {
@@ -103,11 +121,10 @@
       props: {
         div: s,
         data: data,
-        opts: {}
+        opts: {},
       },
     });
   }
-
 </script>
 
 <main>
